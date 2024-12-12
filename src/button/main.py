aed6b82607c00de4e6ba31557aa0e243763f883f
Mathys <mathys.millet@estaca.eu>
2024-12-12 11:04:29 +0100

    Added __main__

--- src/button/main.py
+++ src/button/main.py
@@ -5,7 +5,7 @@ import random
 
 
 def change_color(image):
-    """"
+    """ "
     This function change the color of the background, the size of the button and can make a image appear
     """
 
@@ -28,8 +28,9 @@ def change_color(image):
         image_label.config(image="")
         image_id = 0
 
+
 def center():
-    """"
+    """ "
     This function lock the window width and height and set the window in the middle of the screen
     """
 
@@ -48,34 +49,35 @@ def center():
     root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 
 
-root = tk.Tk()
-root.title("Joli fenêtre")
-root.config(bg="#%06x" % random.randint(0, 0xFFFFFF))
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Joli fenêtre")
+    root.config(bg="#%06x" % random.randint(0, 0xFFFFFF))
 
-center()
+    center()
 
-root.resizable(False, False)  # Can not be resized
-root.attributes("-topmost", 1)  # Always on top of the stack order
+    root.resizable(False, False)  # Can not be resized
+    root.attributes("-topmost", 1)  # Always on top of the stack order
 
-# Place a label on the root window
+    # Place a label on the root window
 
-message = ttk.Label(root, text="Appuie sur le button et je change de couleur")
+    message = ttk.Label(root, text="Appuie sur le button et je change de couleur")
 
-message.pack(padx=20, pady=20)
+    message.pack(padx=20, pady=20)
 
 
-# Read the Image
-image_id = 0
-image = Image.open("/src/button/shrek.png")
-resize_image = image.resize((100, 100))
-img = ImageTk.PhotoImage(resize_image)
+    # Read the Image
+    image_id = 0
+    image = Image.open("/src/button/shrek.png")
+    resize_image = image.resize((100, 100))
+    img = ImageTk.PhotoImage(resize_image)
 
-image_label = ttk.Label(root, image="")
-image_label.pack()
+    image_label = ttk.Label(root, image="")
+    image_label.pack()
 
-button1 = tk.Button(root, text="Cliquez moi", command=lambda: change_color(img))
+    button1 = tk.Button(root, text="Cliquez moi", command=lambda: change_color(img))
 
-button1.place(x=320, y=240, anchor="center")
+    button1.place(x=320, y=240, anchor="center")
 
-# Keep the window displaying
-root.mainloop()
+    # Keep the window displaying
+    root.mainloop()
